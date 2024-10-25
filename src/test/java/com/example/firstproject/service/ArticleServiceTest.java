d342db1b056d4dcc36c49ab230aa8f5b1901aa3d
SonYJ <soja0529@gmail.com>
2024-10-25 12:00:24 +0900

    13장 완료

---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,84 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;                      // test 패키지 임포트
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;       // 앞으로 사용할 수 있는 패키지들 임포트
+
+@SpringBootTest
+class ArticleServiceTest {
+    @Autowired
+    ArticleService articleService;
+
+    // GET
+    @Test           // 해당 메서드가 테스트 코드임을 선언
+    void index() {
+        // 1. 예상 데이터
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
+        // 2. 실제 데이터
+        List<Article> articles = articleService.index();
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
+    }
+    @Test
+    void showSuccess() {
+        // 1. 예상 데이터
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        // 2. 실제 데이터
+        Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+    @Test
+    void showFail() {
+        // 1. 예상 데이터
+        Long id = -1L;
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        assertEquals(expected, article);
+    }
+
+    // POST
+    @Test
+    @Transactional
+    void createSuccess() {
+        // 예상 데이터
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+        // 실제 데이터
+        Article article = articleService.create(dto);
+        // 비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+    @Test
+    @Transactional
+    void createFail() {
+        // 예상 데이터
+        Long id = 4L;
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(id, title, content);
+        Article expected = null;
+        // 실제 데이터
+        Article article = articleService.create(dto);
+        // 비교 및 검증
+        assertEquals(expected, article);
+    }
+}
